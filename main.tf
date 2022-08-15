6dbbd3afce03c42c34567ea7647f167d900eb073
Clemens Wältken <clemens.waltken@microsoft.com>
2022-08-15 11:06:34 +0200

    🔥 add firewall subnet

--- main.tf
+++ main.tf
@@ -35,6 +35,13 @@ resource "azurerm_subnet" "subnet_1_1" {
   address_prefixes     = ["172.16.1.0/24"]
 }
 
+resource "azurerm_subnet" "subnet_1_2" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet_1.name
+  address_prefixes     = ["172.16.2.0/24"]
+}
+
 resource "azurerm_virtual_network" "vnet_2" {
   name                = "vnet-2"
   location            = azurerm_resource_group.example.location
